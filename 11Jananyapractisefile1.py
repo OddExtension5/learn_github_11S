9e744ef9e1194f9e24cdc7b453d7636db95765ed
Ananya-11J <abahinipati12@gmail.com>
2026-05-10 20:24:07 +0530

    Add files via upload

---
+++ 11Jananyapractisefile1.py
@@ -0,0 +1,14 @@
+#mathematical opertors
+print(a+b*c-d**a/c+c//2+d%a)
+#augmented assignment operators
+a,b,c,d=11,12,13,14
+a+=2
+b-=1
+c*=1
+c/=a
+b%=2
+print(a,b,c,d)
+#strings
+h="hello"
+m="to myself"
+print(h+" "+m+"!!!")#+ is concatenation operator
