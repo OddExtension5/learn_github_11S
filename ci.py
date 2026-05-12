510cd7415e8b544c748b67b228e273219e5fd83e
AdrikaBGhub <adrikazorua@gmail.com>
2026-05-12 17:06:48 +0530

    Fix indentation and formatting in ci.py

--- ci.py
+++ ci.py
@@ -2,6 +2,6 @@
 p = float(input("Enter deposited amount:"))
 
 for t in range(1,4):
-    amt = p + (1+0.04)**t
+    amt = p * (1+0.04)**t
     amt = round(amt,2)
     print(f"Amount after {t} years is: {amt}")
